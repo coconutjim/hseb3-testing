414d587c53f7777236e3ca1c63bd05299cfe3902
Lev <osipov.enterprise@gmail.com>
2020-01-17 03:06:46 +0300

    initial

---
+++ AnalaizerClassTest.cs
@@ -0,0 +1,189 @@
+﻿using System;
+using System.Text;
+using System.Collections.Generic;
+using System.Linq;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+using BaseCalculator;
+using System.Collections;
+
+namespace IntegrationTesting
+{
+    [TestClass]
+    public class AnalaizerClassTest
+    {
+        ArrayList arr;
+        public TestContext TestContext { set; get; }
+        private String actual;
+
+        [TestInitialize]
+        public void InitializeMethod()
+        {
+            switch (TestContext.TestName)
+            {
+                case "TestMethod1":
+                    this.IntializeTestMethod1();
+                    break;
+                case "TestMethod2":
+                    this.IntializeTestMethod2();
+                    break;
+                case "TestMethod3":
+                    this.IntializeTestMethod3();
+                    break;
+                case "TestMethod4":
+                    this.IntializeTestMethod4();
+                    break;
+                case "TestMethod5":
+                    this.IntializeTestMethod5();
+                    break;
+                case "TestMethod6":
+                    this.IntializeTestMethod6();
+                    break;
+                case "TestMethod7":
+                    this.IntializeTestMethod7();
+                    break;
+                case "TestMethod8":
+                    this.IntializeTestMethod8();
+                    break;
+                case "TestMethod9":
+                    this.IntializeTestMethod9();
+                    break;
+                case "TestMethod10":
+                    this.IntializeTestMethod10();
+                    break;
+                case "TestMethod11":
+                    this.InitializeTestMethod11();
+                    break;
+                default:
+                    break;
+            }
+            
+        }
+
+        public void IntializeTestMethod1()
+        {
+            AnalaizerClass.expression = "4 + 4 ";
+            actual = "8";
+        }
+
+        public void IntializeTestMethod2()
+        {
+            AnalaizerClass.expression = "16 - 3 ";
+            actual = "13";
+        }
+
+        public void IntializeTestMethod3()
+        {
+            AnalaizerClass.expression = "39 / 3 ";
+            actual = "13";
+        }
+
+        public void IntializeTestMethod4()
+        {
+            AnalaizerClass.expression = "4 * 12 ";
+            actual = "48";
+        }
+
+        public void IntializeTestMethod5()
+        {
+            AnalaizerClass.expression = "50 mod 12 ";
+            actual = "2";
+        }
+
+        public void IntializeTestMethod6()
+        {
+            AnalaizerClass.expression = "m 50 ";
+            actual = "-50";
+        }
+        public void IntializeTestMethod7()
+        {
+            AnalaizerClass.expression = "p 100 ";
+            actual = "100";
+        }
+
+        public void IntializeTestMethod8()
+        {
+            AnalaizerClass.expression = "20 / 0 ";
+            actual = "Error 09";
+        }
+
+        public void IntializeTestMethod9()
+        {
+            AnalaizerClass.expression = Int32.MaxValue.ToString() + " + 1 ";
+            actual = "Error 06";
+        }
+
+        public void IntializeTestMethod10()
+        {
+            AnalaizerClass.expression = Int32.MinValue.ToString() + " - 1 ";
+            actual = "Error 06";
+        }
+
+        public void InitializeTestMethod11()
+        {
+            AnalaizerClass.expression = "12 - 3 + ( 11 * 7 ) ";
+            actual = "86";
+        }
+        private void Test()
+        {
+            arr = AnalaizerClass.CreateStack();
+            AnalaizerClass.opz = arr;
+            String result = AnalaizerClass.RunEstimate();
+            Assert.AreEqual(result, actual);
+        }
+        [TestMethod]
+        public void TestMethod1()
+        {
+            Test();
+        }
+        [TestMethod]
+        public void TestMethod2()
+        {
+            Test();
+        }
+        [TestMethod]
+        public void TestMethod3()
+        {
+            Test();
+        }
+        [TestMethod]
+        public void TestMethod4()
+        {
+            Test();
+        }
+        [TestMethod]
+        public void TestMethod5()
+        {
+            Test();
+        }
+        [TestMethod]
+        public void TestMethod6()
+        {
+            Test();
+        }
+        [TestMethod]
+        public void TestMethod7()
+        {
+            Test();
+        }
+        [TestMethod]
+        public void TestMethod8()
+        {
+            Test();
+        }
+        [TestMethod]
+        public void TestMethod9()
+        {
+            Test();
+        }
+        [TestMethod]
+        public void TestMethod10()
+        {
+            Test();
+        }
+        [TestMethod]
+        public void TestMethod11()
+        {
+            Test();
+        } 
+    }
+}
---
+++ BaseCalculator/UnitTestProjectCalc/AnalaizerClassTest.cs
@@ -0,0 +1,119 @@
+﻿using System;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+using BaseCalculator;
+
+namespace UnitTestProjectCalc
+{
+    [TestClass]
+    public class AnalaizerClassTest
+    {
+
+        /** Constants */
+        long MAXINT = 2147483647;
+
+        /// <summary>
+        /// Основной метод для тестирования.
+        /// Метод тестирует взаимодействие между методами
+        /// CreateStack и RunEstimate класса AnalaizerClass.
+        /// Так как для остальных модулей мы должны поставить заглушки,
+        /// на вход этих методов будут поступать выражения, которые соотвествуют
+        /// проверкам методов CheckCurrency и Format класса AnalaizerClass.
+        /// ПРИМЕЧАНИЕ. Тесты на случаи, когда после выполнения CreateStack
+        /// поле opz класса AnalaizerClass будет равно null,  мы не пишем,
+        /// так как структура программы не предусматривает взаимодействия
+        /// этих методов при таких входных данных.
+        /// </summary>
+        /// <param name="expression">входное выражение, удовлетворяющее
+        /// постусловиям методов CheckCurrency и Format, после обработки
+        /// этого выражения в методе CreateStack поле opz не должно быть null</param>
+        /// <param name="expected">ожидаемый результат</param>
+        private void Test_CreateStack_RunEstimate(string expression, string expected)
+        {
+            AnalaizerClass.expression = expression;
+            AnalaizerClass.opz = AnalaizerClass.CreateStack();
+            string actual = AnalaizerClass.RunEstimate();
+            Assert.AreEqual(expected, actual);
+        }
+
+        [TestMethod]
+        public void Test_Addition()
+        {
+            Test_CreateStack_RunEstimate("5 + 50 ", Convert.ToString(5 + 50));
+        }
+
+        [TestMethod]
+        public void Test_Substraction()
+        {
+            Test_CreateStack_RunEstimate("123 - 11 ", Convert.ToString(112));
+        }
+
+        [TestMethod]
+        public void Test_Multiplication()
+        {
+            Test_CreateStack_RunEstimate("45 * 9 ", Convert.ToString(45 * 9));
+        }
+
+        [TestMethod]
+        public void Test_Division()
+        {
+            Test_CreateStack_RunEstimate("100 / 10 ", Convert.ToString(100 / 10));
+        }
+
+        [TestMethod]
+        public void Test_Modulo()
+        {
+            Test_CreateStack_RunEstimate("34 mod 5 ", Convert.ToString(34 % 5));
+        }
+
+        [TestMethod]
+        public void Test_Unary_Plus()
+        {
+            Test_CreateStack_RunEstimate("p 378 ", Convert.ToString(378));
+        }
+
+        [TestMethod]
+        public void Test_Unary_Minus()
+        {
+            Test_CreateStack_RunEstimate("m 721 ", Convert.ToString(-721));
+        }
+
+        [TestMethod]
+        public void Test_Braces()
+        {
+            Test_CreateStack_RunEstimate("( 45 - 25 ) / 4 ", Convert.ToString((45 - 25) / 4));
+        }
+
+        [TestMethod]
+        public void Test_Overflow()
+        {
+            Test_CreateStack_RunEstimate(Convert.ToString(MAXINT) + " + 100 ", "Error 06");
+        }
+
+        [TestMethod]
+        public void Test_Zero_Division()
+        {
+            Test_CreateStack_RunEstimate("25 / 0 ", "Error 09");
+        }
+
+        [TestMethod]
+        public void Test_Empty_Expression()
+        {
+            Test_CreateStack_RunEstimate("", "Error 03"); // этот случай проходит через 
+                                  // проверки методов CheckCurrency и Format
+        }
+
+        [TestMethod]
+        public void Test_Numbers_Only()
+        {
+            Test_CreateStack_RunEstimate("6456 ", "6456"); // этот случай проходит через 
+                                   // проверки методов CheckCurrency и Format
+        }
+
+        [TestMethod]
+        public void Test_One_Space_Expression()
+        {
+            Test_CreateStack_RunEstimate(" ", ""); // этот случай проходит через 
+                           // проверки методов CheckCurrency и Format
+        }
+    }
+}
